f8d0a945a5a098a465717a2e6f08b11e5cdf2f99
linwenjun <linwenjun@163.com>
2019-01-13 21:29:57 +0800

    change docker exec to docker-compose exec

--- run.sh
+++ run.sh
@@ -4,4 +4,5 @@ JAR_NAME=data-engineering-workshop-assembly-0.1.jar
 
 sbt assembly
 cp ./target/scala-2.11/$JAR_NAME ./jars
-docker exec -it spark-basic_spark_1 bash -c "./bin/spark-submit --class workshop.wordcount.WordCount --master local[4] /jars/data-engineering-workshop-assembly-0.1.jar"
+
+docker-compose exec spark bash -c "./bin/spark-submit --class workshop.wordcount.WordCount --master local[4] /jars/data-engineering-workshop-assembly-0.1.jar"
